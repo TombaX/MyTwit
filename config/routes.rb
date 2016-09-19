1ce67c728bdc4f45206db26d354e53e96a3d897d
TombaX <tomba000@mail.ru>
2016-09-19 20:51:48 +0300

    Finish layout, add 'Sign up'

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  get   'users/new'
 
-  get 'static_pages/about'
+  root  'static_pages#home'
 
-  get 'static_pages/contacts'
+  match '/signup',   to: 'users#new',              via: 'get'
+  
+  match '/help',     to: 'static_pages#help',      via: 'get'
+  
+  match '/about',    to: 'static_pages#about',     via: 'get'
+  
+  match '/contacts', to: 'static_pages#contacts',  via: 'get'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,57 +2,40 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Mytwit Sample App" }
+  include Rails.application.routes.url_helpers
+
+    subject { page }
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    before { visit root_path }
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+    it { should have_content('Home') }
+    it { should have_title("Home") }
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before {visit help_path}
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it {should have_content('Help')}
+    it {should have_title("Help")}
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before {visit about_path}
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it {should have_content('About Us')}
+    it {should have_title("About Us")}
   end
 
-  describe "Contacts" do
+  describe "Contact page" do
 
-    it "should have the content 'Contacts'" do
-      visit '/static_pages/contacts'
-      expect(page).to have_content('Contacts')
-    end
-
-    it "should have the title 'Contacts'" do
-      visit '/static_pages/contacts'
-      expect(page).to have_title("#{base_title} | Contacts")
-    end
+    before {visit contacts_path}
+    
+    it {should have_content('Contacts')}
+    it {should have_title("Contacts")}
   end
+
 end
